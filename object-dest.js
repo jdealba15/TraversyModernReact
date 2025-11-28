0f4e92f71ee613cb0a15699334ae6904fec15b78
jdealba15 <jdealba15@yahoo.com>
2025-11-28 08:23:51 -0800

    Object Destructure Study

---
+++ object-dest.js
@@ -0,0 +1,20 @@
+const note = {
+    title: "Meeting Notes",
+    content: 'Discuss project roadmap',
+    isPinned: true,
+};
+
+const { title } = note;
+
+console.log(title);
+
+
+// next task, perhaps find way to integrate a template literal?
+
+const newNote = `
+    title: ${note.title}
+    content: ${note.content + ' (Please add any comments or thoughts here).'}
+    status: ${note.isPinned ? '📌 Special note' : 'Regular Note'}
+`;
+
+console.log(newNote);
