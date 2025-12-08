8edc14174fa803fbaf8e975b0d2e1bd117e26df3
jdealba15 <jdealba15@yahoo.com>
2025-12-08 09:21:51 -0800

    study, 'Spread Operators'

---
+++ spreadOp.js
@@ -0,0 +1,8 @@
+const notes = ['Meeting Notes', 'Grocery List'];
+notes.push('Workout Plan');
+console.log(notes);
+
+const slice = notes.slice(1, 2);
+console.log(slice);
+
+const splice = notes.splice
